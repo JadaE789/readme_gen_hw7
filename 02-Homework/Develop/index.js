4433f6bfec1c2b42c8fc7df40e2e237f1bcab6a0
Jada Desormeaux <JadaE789@gmail.com>
2021-04-18 15:28:19 -0400

    questions added and working in terminal

--- 02-Homework/Develop/index.js
+++ 02-Homework/Develop/index.js
@@ -1,13 +1,96 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+function promptQuestions () {
+  return inquirer.prompt ([
+    {
+        type: "input",
+        name: "title",
+        message: "Title of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Brief description of Project",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Installation Requirements?",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Which license used for the Project?",
+        choices:
+        [
+            "Apache license 2.0",
+            "GNU General Public License v3.0",
+            "MIT",
+            "Unlicensed"
+        ],
+        default: 2
+    },
+    {
+        type: "input",
+        name: "contribute",
+        message: "Contributions for this Project",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Link to Demo Video",
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Type your github username?",
+        validate: function(answer)
+        { 
+            if(answer === "") {
+                return "You must enter your github username";
+            }
+            return true;
+        }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+        validate: function(answer)
+        { 
+            if(answer === "") {
+                return "You must enter email address";
+            }
+            return true;
+        }
+    }
+  ]);
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+    console.log("Welcome to readme file generator")
+    try{
+        const data = await promptQuestions();
+        const license = data.license;
+        const badge = await renderBadge(license);
+        const readme = generateMarkdown(data);
+        await writeFileAsync("Prof_ReadMe.md",readme);
+        console.log("readme.MD succesfully generated");
+    } catch(err) {
+      console.log(err);
+    }
+}
 
 // Function call to initialize app
 init();
--- 02-Homework/Develop/utils/generateMarkdown.js
+++ 02-Homework/Develop/utils/generateMarkdown.js
@@ -1,20 +1,68 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+//License
+async function renderBadge(license){
+  let badge = "";
+  switch(license){
+      case "Apache license 2.0":
+          return badge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+      case "GNU General Public License v3.0":
+          return badge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+      case "MIT":
+          return badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      case "The Unlicense":
+          return badge = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)";
+      default:
+          console.log("Render badge failed");
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Table of Contents
+
+  * [Project Description](#project-description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Demo Video](#Project-Demo-Video)
+  * [Questions?](#questions)
+  
+  ## Project Description
+  
+  ${data.description}
+  
+  ## Installation
+  
+  ${data.installation}
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## License
+  
+  This project is under ${data.license} for licensing.
+  
+  ${badge}
+  
+  ## Contributing
+  
+  ${data.contribute}
+  
+  ## Project Demo Video
+  
+  ${data.test}
+  
+  ## Questions
+  
+  please feel free to ask me any questions.
+  
+  Github username: ${data.username}
+  
+  Github Email: <${data.email}>
+ `;
 }
 
 module.exports = generateMarkdown;
